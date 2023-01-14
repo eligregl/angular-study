f0448137e171467f4ac36f8fc2054a5408b95ef2
eligregl <eligregl@gmail.com>
2023-01-14 17:21:20 -0500

    ngOnDestroy

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,16 +1,52 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, OnChanges, AfterViewInit, OnDestroy, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss']
 })
-export class ImgComponent {
+export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy {
 
   @Input() img: string = '';
   @Output() loaded = new EventEmitter<string>();
-
   imgDefault = "./assets/images/album.jpg";
+  counter = 0;
+  counterFn: number | undefined;
+
+  constructor() {
+    // before render
+    // NO async -- once time
+    console.log('constructor', 'imgValue =>', this.img);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    // before - during render
+    // changes inputs -- multiples times
+    console.log('ngOnChanges', 'imgValue =>', this.img);
+    console.log(changes);
+  }
+
+  ngOnInit(): void {
+    // before render
+    // async - fetch -- once time
+    console.log('ngOnInit', 'imgValue =>', this.img);
+    this.counterFn = window.setInterval(() => {
+      this.counter += 1;
+      console.log('Counter running')
+    }, 1000)
+  }
+
+  ngAfterViewInit() {
+    // after render
+    // handler children -- once time
+    console.log('ngAfterViewInit');
+  }
+
+  ngOnDestroy() {
+    // delete -- once time
+    console.log('ngOnDestroy');
+    window.clearInterval(this.counterFn);
+  }
 
   imgError() {
     this.img = this.imgDefault;
